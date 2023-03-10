abcf2453f029aceac42693d4f051cd7c47348a48
awscloudgirl <awscloudgirl@gmail.com>
2023-03-10 02:49:54 +0000

    Took out mysterious code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -36,10 +36,6 @@ provider.add_span_processor(processor)
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
-# Show this in the logs withing the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
-
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
